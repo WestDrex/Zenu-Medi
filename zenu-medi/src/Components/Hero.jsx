a2a7f4c52185f6379fdeff7d5423dbf09748cc37
ErekRaph <westdrex@gmail.com>
2024-06-21 23:04:37 +0100

    Initial commit

---
+++ zenu-medi/src/Components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../Components/Hero.css";
+
+export default function Hero() {
+  return (
+    <div className="hero-container">
+      <h1 className="intro">Mindful Breathing & meditation</h1>
+      <p className="intro-content">
+        Aao hum sab mil ke kaafi achhe saanse lete hai kyuki agar hum saanse
+        nahi lenge to mar jaayenge, Isliye sabko saanse to leni hi chahiye.
+        samjhe ki nhi...huhhh.
+      </p>
+      <p className="know-more">Know more</p>
+      <hr />
+    </div>
+  );
+}
